2707eb8934dc471c0ec6bd5f2fa4284804aa7beb
13542434 <13542434@student.uts.edu.au>
2023-09-04 13:48:34 +1000

    first commit

---
+++ build/CMakeFiles/sprint2_sim1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sprint2_sim1.dir/src/sprint2_sim1.cpp.o"
+  "devel/lib/rs1/sprint2_sim1.pdb"
+  "devel/lib/rs1/sprint2_sim1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sprint2_sim1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rs1/catkin_ws/src/rs1
+# Build directory: /home/rs1/catkin_ws/src/rs1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
