4928e6410c086b37b24bb194d7a09cbebaa28519
13542434 <13542434@student.uts.edu.au>
2023-10-09 13:57:26 +1100

    hazards start

---
+++ hazards.py
@@ -0,0 +1,15 @@
+import rospy
+import actionlib
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+while __name__ == "__main__":
+    heat_points = [
+        {
+            'x': 4.0,
+            'y': -2.0,
+            'z': 0.0,
+        }
+    ]
+
+
