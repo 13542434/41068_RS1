05a796227790454d3a8f533befb0dcd08a5cafe3
Chris Gilmore <cjg.topey@gmail.com>
2023-10-15 22:34:47 +1100

    Change hazards to be a class to be later used by the program

--- hazards.py
+++ hazards.py
@@ -5,36 +5,61 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from nav_msgs.msg import Odometry
 import math
 
-# Global variable to store the robot's current position
-current_position = None
 
-def odom_callback(data):
-    global current_position
-    current_position = data.pose.pose.position
-
-def calculate_distance(point1, point2):
-    return math.sqrt((point2['x'] - point1.x)**2 + (point2['y'] - point1.y)**2)
-
-if __name__ == "__main__":
-    rospy.init_node('distance_to_heat_point')
-
-    # Subscribe to the /odom topic to get the robot's current position
-    rospy.Subscriber("/odom", Odometry, odom_callback)
-
-    # Wait for a moment to ensure we get the robot's position
-    rospy.sleep(1)
-
+class Hazards:
+    # Global variable to store the robot's current position
+    current_position = None
+    distance_threshold = 1.0 # Distance between robot and hazard to call hazard
     heat_points = [
         {
             'x': 4.0,
             'y': -2.0,
             'z': 0.0,
+            'temp': 50.0,
         }
     ]
+    
+    def odom_callback(self, data):
+        self.current_position = data.pose.pose.position
 
-    if current_position:
-        for heat_point in heat_points:
-            distance = calculate_distance(current_position, heat_point)
-            print(f"Distance to heat_point ({heat_point['x']}, {heat_point['y']}) is: {distance:.2f} meters")
-    else:
+    def calculate_distance(self, point) -> float:
+        return math.sqrt((point['x'] - self.current_position.x)**2 + (point['y'] - self.current_position.y)**2)
+    
+    def check_hazard(self) -> list:
+        result : list = []
+        for heat_point in hazards.heat_points:
+            distance = self.calculate_distance(heat_point)
+            if distance < self.distance_threshold:
+                result.append(heat_point['temp'])
+        return result
+                
+    
+    def __init__(self) -> None:
+        rospy.init_node('distance_to_heat_point')
+
+        # Subscribe to the /odom topic to get the robot's current position
+        rospy.Subscriber("/odom", Odometry, self.odom_callback)
+
+
+
+
+if __name__ == "__main__":
+    # Use this as a test function
+    hazards = Hazards() 
+    # Wait for a moment to ensure we get the robot's position
+    rospy.sleep(1)
+
+    if not hazards.current_position:
         print("Failed to get the robot's current position!")
+        exit(1)
+
+    detected_hazards = hazards.check_hazard()
+    
+    if len(detected_hazards) > 0:
+        for temperature in detected_hazards:
+            print(f"Hazard detected at {hazards.current_position.x},{hazards.current_position.y} of {temperature}C")
+    else:
+        print("No hazards detected at current position")
+        
+# print(f"Distance to heat_point ({heat_point['x']}, {heat_point['y']}) is: {distance:.2f} meters")
+        
